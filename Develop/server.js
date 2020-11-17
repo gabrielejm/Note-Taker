44c5f63a542372aec1ab880531e1870eecceaae5
Justin Gabriele <gabrielejustin@yahoo.com>
2020-11-17 17:06:09 -0500

    added path

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,27 @@
+// Dependencies
 const express = require("express");
 const fs = require("fs");
+const path = path("path");
 
+// Sets up the Express App
 const app = express();
 const PORT = 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+
+app.get("/notes", function (req, res) {
+  res.send();
+});
+
+app.get("*", function (req, res) {
+  res.send();
+});
+
+// Listener
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
