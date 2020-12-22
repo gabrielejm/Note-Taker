ccbb60b5045420ee1b9f30abd4253e41caff6708
Justin Gabriele <gabrielejustin@yahoo.com>
2020-12-22 17:38:03 -0500

    updated routing due to change in directories

---
+++ server.js
@@ -0,0 +1,51 @@
+// Dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
+const db = require("./Develop/db/db.json");
+
+// Sets up the Express App
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const notes = [{}];
+
+// Routes
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+
+// Shows all notes json format
+app.get("/api/notes", function (req, res) {
+  return fs.readFile("/Develop/db/db.json");
+});
+
+// Adds new note
+app.post("/api/notes", function (req, res) {
+  const newNote = req.body;
+  uuidv4(newNote);
+  notes.push(newNote);
+  fs.writeFileSync();
+  res.sendFile("/Develop/db/db.json");
+});
+
+// Deletes note
+app.delete("/api/notes/:id", function (req, res) {
+  res.send("/Develop/db/db.json");
+});
+
+// Listener
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
