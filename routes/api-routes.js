147c86a7b1b668874aeed52d270c6f61a0998491
Justin Gabriele <gabrielejustin@yahoo.com>
2020-12-28 20:32:04 -0500

    more routing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,24 +1,30 @@
 const router = require("express").Router();
 const path = require("path");
 const fs = require("fs");
+const uuidv1 = require("uuid/v1");
 
 // Shows all notes json format
 router.get("/notes", function (req, res) {
-  return fs.readFile("./db/db.json");
+  const data = fs.readFile("./db/db.json");
+  res.json(data);
 });
 
 // Adds new note
 router.post("/notes", function (req, res) {
-  const newNote = req.body;
-  uuidv4(newNote);
+  const title = req.body.title;
+  const text = req.body.text;
+  const newNote = { title, text, id: uuidv1() };
   notes.push(newNote);
-  fs.writeFileSync();
-  res.sendFile("./db/db.json");
+  fs.writeFileSync(notes);
+  res.json(newNote);
 });
 
 // Deletes note
 router.delete("/notes/:id", function (req, res) {
-  res.send("./db/db.json");
+  const data = fs.readFile("./db/db.json");
+  const updatedNotes = data.filter((note) => note.id !== req.params.id);
+  fs.writeFileSync(Updatednotes);
+  res.json({ ok: true });
 });
 
 module.exports = router;
