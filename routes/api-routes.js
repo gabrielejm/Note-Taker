2e1f31ca101658a1f476982e3e6585ccaea758c9
Justin Gabriele <gabrielejustin@yahoo.com>
2020-12-28 19:39:07 -0500

    fixed api routes?

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,12 +3,12 @@ const path = require("path");
 const fs = require("fs");
 
 // Shows all notes json format
-router.get("/notes", function (req, res) {
+router.get("/api/notes", function (req, res) {
   return fs.readFile("./db/db.json");
 });
 
 // Adds new note
-router.post("/notes", function (req, res) {
+router.post("/api/notes", function (req, res) {
   const newNote = req.body;
   uuidv4(newNote);
   notes.push(newNote);
@@ -17,7 +17,7 @@ router.post("/notes", function (req, res) {
 });
 
 // Deletes note
-router.delete("/notes/:id", function (req, res) {
+router.delete("/api/notes/:id", function (req, res) {
   res.send("./db/db.json");
 });
 
